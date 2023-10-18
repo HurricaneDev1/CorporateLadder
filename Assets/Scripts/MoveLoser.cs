4bd3b8d91b42740b8bed60f01257d40be6ac49be
MonkeyNinja2296 <grantcornaby@gmail.com>
2023-10-17 23:14:42 -0600

    Cyoty time and better floor detecttion and accleration

--- Assets/Scripts/MoveLoser.cs
+++ Assets/Scripts/MoveLoser.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class MoveLoser : MonoBehaviour
 {
-    private Vector2 vel = new Vector2(0,0);
+    [SerializeField] private Vector2 vel = new Vector2(0,0);
     private Rigidbody2D rb;
     public int speed = 5;
 
@@ -16,6 +16,12 @@ public class MoveLoser : MonoBehaviour
     [SerializeField] private bool grounded = false;
     [SerializeField] private float footCircleSize = .3f;
     [SerializeField]private LayerMask groundLayer;
+    private float tempVelX = 0f;
+    [SerializeField] private float drag = 1.05f;
+    [SerializeField] private float accleration = 10f;
+    private bool gravityBool = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,18 +31,36 @@ public class MoveLoser : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(vel.x*speed,vel.y);
-        if(grounded == false){
+        if(Mathf.Abs(tempVelX) < 1 || Mathf.Abs(tempVelX)/tempVelX != Mathf.Abs(vel.x)/vel.x){
+            tempVelX += vel.x/accleration;
+        }
+
+        if(vel.x == 0){
+            tempVelX /= drag;
+        }
+
+        rb.velocity = new Vector2(tempVelX*speed,vel.y);
+
+        if(gravityBool == true){
             vel = new Vector2(vel.x, vel.y - gravity);
         } else {
             vel = new Vector2(vel.x,0);
         }
+
         if(!moving){
-            vel = new Vector2(vel.x/1.05f,vel.y);
+            vel = new Vector2(0,vel.y);
         }
-        if(Physics2D.CircleCast(point.position, footCircleSize, new Vector2(0,0),footCircleSize, groundLayer)){
-            grounded = true;
+
+        if(vel.y <= 0){
+            if(Physics2D.CircleCast(point.position, footCircleSize, new Vector2(0,0),footCircleSize, groundLayer)){
+                grounded = true;
+                gravityBool = false;
+            } else {
+                StartCoroutine(cyotyTime(.2f));
+                gravityBool = true;
+            }
         }
+        
     }
     public void walk(InputAction.CallbackContext context){
         if(context.action.triggered){
@@ -54,6 +78,7 @@ public class MoveLoser : MonoBehaviour
         if(context.action.triggered && grounded){
             vel = new Vector2(vel.x, jumpSpeed);
             grounded = false;
+            gravityBool = true;
         } 
         
     }
@@ -62,5 +87,9 @@ public class MoveLoser : MonoBehaviour
     }
     private void OnDrawGizmos() {
         Gizmos.DrawWireSphere(point.position,footCircleSize);
+    }
+    private IEnumerator cyotyTime(float time){
+        yield return new WaitForSeconds(time);
+        grounded = false;
     }
 }
